75cec3d1cc30076460b77c5d51be0aaee42efc20
Lucas Monteiro <lucasmonteirob1@outlook.com>
2022-09-22 16:25:48 -0300

    derrapagem

---
+++ derrapagem.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  // Nao deletar
+  final line = stdin.readLineSync() ?? '';
+  if (line.isEmpty) exit(0);
+  final inputs = line.split(' ');
+
+  // Variaveis iniciais
+  final velocidade = int.parse(inputs[0]);
+  final aceleracao = int.parse(inputs[1]);
+
+  // Escreva seu programa a partir daqui
+  final distancia = -(pow(velocidade, 2)) / (2 * aceleracao);
+  print(distancia);
+}
