ef297d05c70a8043aa8579e28c47d583429997e0
Lucas Monteiro <lucasmonteirob1@outlook.com>
2022-10-03 12:58:14 -0300

    upando exercicio

--- logicadart2/m01_A07_05_fibonacci.dart
+++ logicadart2/m01_A07_05_fibonacci.dart
@@ -1,16 +1,28 @@
-int fib(int n) {
-  if (n <= 1) {
-    return n;
-  }
-  return fib(n - 1) + fib(n - 2);
-}
+// int fib(int n) {
+//   if (n <= 1) {
+//     return n;
+//   }
+//   return fib(n - 1) + fib(n - 2);
+// }
 
 void main() {
-  final fibo = 10;
-  String saida = '';
+  final input = 150;
+  fib(0, 0, 1, input);
 
-  for (int i = 1; i <= fibo; i++) {
-    saida += fib(i).toString() + " ";
+  // for (int i = 1; i <= input; i++) {
+  //   output += fib(i).toString() + ' ';
+  // }
+  print(result);
+}
+
+String result = '';
+
+void fib(int valorAtual, int valorAnterior, int fibAtual, int valorInformado) {
+  result += '$fibAtual ';
+  valorAnterior = valorAtual;
+  valorAtual = fibAtual;
+  fibAtual = valorAnterior + valorAtual;
+  if (fibAtual <= valorInformado) {
+    fib(valorAtual, valorAnterior, fibAtual, valorInformado);
   }
-  print(saida);
 }
