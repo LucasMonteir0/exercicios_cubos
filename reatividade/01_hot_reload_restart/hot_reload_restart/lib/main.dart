1ce55aea0d457a6a3b3b002e1b80ef010d1bba86
Lucas Monteiro <lucasmonteirob1@outlook.com>
2022-10-04 21:57:44 -0300

    upando exercicios

---
+++ reatividade/01_hot_reload_restart/hot_reload_restart/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Card(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.blueAccent, Colors.purpleAccent],
+            ),
+          ),
+          child: Center(
+            child: Text(
+              'Flutter',
+              style: TextStyle(
+                fontSize: 32.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+// class App extends StatelessWidget {
+//   const App({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: Card(
+//         color: Colors.pinkAccent,
+//         child: Center(
+//           child: Text(
+//             'Flutter',
+//             style: TextStyle(fontSize: 32, color: Colors.white),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
