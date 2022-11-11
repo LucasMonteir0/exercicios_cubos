8d0b4c4c2c933bac2204d2e9f8635251fcb5775e
Lucas Monteiro <lucasmonteirob1@outlook.com>
2022-11-10 21:30:25 -0300

    upando exercicio 4

--- data_inputs/lib/exercices/ex04_random_focus.dart
+++ data_inputs/lib/exercices/ex04_random_focus.dart
@@ -1,31 +1,114 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MyHomePage());
+    return MaterialApp(
+        theme: ThemeData(
+            appBarTheme: const AppBarTheme(backgroundColor: Color(0xFF195F1E)),
+            textTheme: const TextTheme(
+                bodyText1: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+                bodyText2: TextStyle(
+                    color: Color(0xFF195F1E),
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold)),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color(0xFF195F1E))))),
+        home: const MyHomePage());
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final focusController1 = FocusNode();
+
+  final focusController2 = FocusNode();
+
+  final focusController3 = FocusNode();
+
+  final focusController4 = FocusNode();
+
+  final focusController5 = FocusNode();
+  int random = 0;
+  @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          TextFormField(),
-          TextFormField(),
-          TextFormField(),
-          TextFormField(),
-          TextFormField(),
-        ],
+      appBar: AppBar(
+        title: Text(
+          'Random Focus',
+          style: theme.textTheme.bodyText1,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Text('Sorteado: ${random + 1}'),
+            ),
+            TextFormField(
+              focusNode: focusController1,
+            ),
+            TextFormField(
+              focusNode: focusController2,
+            ),
+            TextFormField(
+              focusNode: focusController3,
+            ),
+            TextFormField(
+              focusNode: focusController4,
+            ),
+            TextFormField(
+              focusNode: focusController5,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: SizedBox(
+                width: 500,
+                child: ElevatedButton(
+                    onPressed: () {
+                      random = Random().nextInt(5);
+                      setState(() {});
+                      if (random == 0) {
+                        focusController1.requestFocus();
+                      } else if (random == 1) {
+                        focusController2.requestFocus();
+                      } else if (random == 2) {
+                        focusController3.requestFocus();
+                      } else if (random == 3) {
+                        focusController4.requestFocus();
+                      } else if (random == 4) {
+                        focusController5.requestFocus();
+                      }
+                    },
+                    child: Text(
+                      'Focus Random',
+                      style: theme.textTheme.bodyText1,
+                    )),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
